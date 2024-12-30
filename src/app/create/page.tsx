a18f8bc108540f9eee303a82196dd4112f9cf769
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-12-30 11:20:57 +0530

    New changes

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -283,9 +283,7 @@ function JalDristi() {
             </Alert>
 
             {/* Submit */}
-            <Button
-              type="submit"
-              className="w-full bg-green-600 hover:bg-green-700 transition-colors duration-200"
+            <Button type="submit" className="w-full bg-green-600 hover:bg-green-700 transition-colors duration-200"
               disabled={isSubmitting}
             >
               {isSubmitting ? (
